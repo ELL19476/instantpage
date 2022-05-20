958e09348a58bfe20c4fdc27145167d55ba9cb12
Konrad Hapke <hap19520@spengergasse.at>
2022-05-20 12:20:11 +0200

    startscreen into component

--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,9 @@
 <script lang="ts">
-import Button from "./components/basic/Button.svelte";
-import Title from "./components/Title.svelte";
-import Center from "./components/basic/Center.svelte";
-import Panel from "./components/Panel.svelte";
-import wpLogo from "./assets/startpage/wp-logo.svg"
-import moneyReturn from "./assets/startpage/money-return.svg"
-import Hr from "./components/basic/Hr.svelte";
+import SiteHeader from "./SiteHeader.svelte";
+
 
 </script>
 
 <main>
-    <Title class="mt-12">IHRE <b>PROFESSIONELLE</b>, <b>HANDGEMACHTE</b> WEBSITE AUS WIEN</Title>
-    
-    <Center class="mt-14 space-x-5" horizontal={false}>
-        <Panel
-            header="AB"
-            subheader="85% billiger"
-            text="als die Konkurrenz"
-            contentWidth="100"
-        >
-            <Title>129€</Title>
-        </Panel>
-        <Panel
-            header="100% EDITIERBAR"
-            subheader="Wordpress Support"
-            text="Inhalte selbst schreiben und austauschen"
-        >
-            {@html wpLogo}
-        </Panel>
-        <Panel
-            header="GELD-ZURÜCK GARANTIE"
-            subheader="Nicht zufrieden?"
-            text="Sie bekommen einfach Ihr Geld zurück!"
-            contentWidth="50"
-        >
-            {@html moneyReturn}
-        </Panel>
-    </Center>
-    <Hr color="panel/40" class="my-12" width="2/3"></Hr>
+    <SiteHeader />
 </main>
---
+++ src/SiteHeader.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+import Center from "./components/basic/Center.svelte";
+import Panel from "./components/Panel.svelte";
+
+import wpLogo from "./assets/startpage/wp-logo.svg"
+import moneyReturn from "./assets/startpage/money-return.svg"
+
+import Title from "./components/Title.svelte";
+import Hr from "./components/basic/Hr.svelte";
+import Button from "./components/basic/Button.svelte";
+
+
+
+</script>
+
+<div>
+    <Title class="mt-12">IHRE <b>PROFESSIONELLE</b>, <b>HANDGEMACHTE</b> WEBSITE AUS WIEN</Title>
+    
+    <Center class="overflow-x-scroll mt-14 space-x-5" horizontal={false}>
+        <Panel
+            header="AB"
+            subheader="85% billiger"
+            text="als die Konkurrenz"
+            contentWidth="100"
+        >
+            <Title>129€</Title>
+        </Panel>
+        <Panel
+            header="100% EDITIERBAR"
+            subheader="Wordpress Support"
+            text="Inhalte selbst schreiben und austauschen"
+        >
+            {@html wpLogo}
+        </Panel>
+        <Panel
+            header="GELD-ZURÜCK GARANTIE"
+            subheader="Nicht zufrieden?"
+            text="Sie bekommen einfach Ihr Geld zurück!"
+            contentWidth="50"
+        >
+            {@html moneyReturn}
+        </Panel>
+    </Center>
+    <Hr color="panel/40" class="my-12" width="2/3"></Hr>
+    <Center class="space-x-5">
+        <Button>jetzt loslegen</Button>
+        <Button mainColor="accent-1">mehr erfahren</Button>
+    </Center>
+</div>
